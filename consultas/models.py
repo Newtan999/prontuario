ebe1420cb9a5dd5142d2210bb77565a46261bfe8
Newton Filho <jnewton.fff@gmail.com>
2025-04-06 21:07:05 -0300

    Cria o modeo de Consulta com campos principais e relação com Paciente

--- consultas/models.py
+++ consultas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from pacientes.models import Pacientes
 
-# Create your models here.
+class Consulta(models.Model):
+    paciente = models.ForeignKey(Pacientes, on_delete=models.PROTECT)
+    data = models.DateTimeField(auto_now_add=True)
+    motivo = models.TextField(blank=True)
+    evolucao = models.TextField()
+    prescricao = models.TextField()
+
+    def __str__(self):
+        return f'Consulta de {self.paciente.nome} em {self.data.strftime("%d%m%y %H:%M")}'
